093868f1a272798ae8ea56edacaff1e2d5d77201
hkaa926 <thomaschun01@gmail.com>
2026-01-18 19:23:23 +0900

    과제1

--- avatar_vision/avatar_vision/rgb_sub.py
+++ avatar_vision/avatar_vision/rgb_sub.py
@@ -1,38 +1,45 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import CompressedImage # 메시지 타입 변경
+from sensor_msgs.msg import CompressedImage
+
+# OpenCV 관련
 import cv2
 import numpy as np
 
-class RealSenseCompressedSubscriber(Node):
+# Subscriber 노드 생성
+class RealSenseRGBSubscriber(Node):
     def __init__(self):
-        super().__init__('realsense_rgb_subscriber')
+        super().__init__('realsense_rgb_subscriber') # 노드 이름
 
         self.subscription = self.create_subscription(
-            CompressedImage, # 타입 변경
-            '/realsense/color/image_raw/compressed', # 토픽 이름 일치
+            CompressedImage,
+            '/realsense/color/image_raw/compressed',   # Publisher와 동일
             self.callback,
             10
         )
-        self.get_logger().info('Subscribed to Compressed Image')
 
+        self.get_logger().info(
+            'Subscribed to /realsense/color/image_raw/compressed'
+        )
+
+# 구독시 할 일
     def callback(self, msg):
-        # 1. 압축된 바이트 데이터를 numpy 배열로 변환
-        np_arr = np.frombuffer(msg.data, np.uint8)
-        
-        # 2. OpenCV를 이용해 JPEG 압축 해제 (디코딩)
-        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        try:
+            np_arr = np.frombuffer(msg.data, np.unit8)
 
-        if frame is not None:
-            # 영상 화면 띄우기
-            cv2.imshow('Remote View (Compressed)', frame)
-            cv2.waitKey(1)
-        else:
-            self.get_logger().warning('Failed to decode image')
+            frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            if frame is not None:
+                cv2.imshow('RealSense Compressed RGB (subscriber)', frame)
+                cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f'Could not decode image: {e}')
+      
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = RealSenseCompressedSubscriber()
+    node = RealSenseRGBSubscriber()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
@@ -41,6 +48,7 @@ def main(args=None):
         node.destroy_node()
         rclpy.shutdown()
         cv2.destroyAllWindows()
+  
 
 if __name__ == '__main__':
     main()
