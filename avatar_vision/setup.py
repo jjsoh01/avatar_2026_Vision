3315323777c22df4dd54ce1c20c751f75e6fc574
ukh0415 <ukh0415@gmail.com>
2026-01-20 14:19:18 +0900

    Add webcam publisher

--- avatar_vision/setup.py
+++ avatar_vision/setup.py
@@ -26,6 +26,7 @@ setup(
         'console_scripts': [
             'rgb_subscriber = avatar_vision.rgb_sub:main',
             'realsense_rgb_publisher = avatar_vision.camera_test:main',
+            'webcam_publisher = avatar_vision.camera_test_webcam:main'
         ],
     },
 )
