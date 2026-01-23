bd0c8303b753f72ae06f3caf1873517fbafc43f2
hayoung <thomaschun01@gmail.com>
2026-01-23 16:36:44 +0900

    20260123 hayoung

--- avatar_vision/avatar_vision/rgb_sub.py
+++ avatar_vision/avatar_vision/rgb_sub.py
@@ -1,48 +1,54 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 
 # OpenCV 관련
-from cv_bridge import CvBridge
 import cv2
-
+import numpy as np
 
 # Subscriber 노드 생성
 class RealSenseRGBSubscriber(Node):
     def __init__(self):
         super().__init__('realsense_rgb_subscriber') # 노드 이름
 
-        self.bridge = CvBridge() # 이번엔 ROS Image -> OpenCV 로 바꾸려고
-
         self.subscription = self.create_subscription(
-            Image,
-            '/realsense/color/image_raw',   # Publisher와 동일
+            CompressedImage,
+            '/realsense/color/image_raw/compressed',   # Publisher와 동일
             self.callback,
             10
         )
 
         self.get_logger().info(
-            'Subscribed to /realsense/color/image_raw'
+            'Subscribed to /realsense/color/image_raw/compressed'
         )
 
 # 구독시 할 일
     def callback(self, msg):
-        # ROS Image → OpenCV 변환
-        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        try:
+            np_arr = np.frombuffer(msg.data, np.uint8)
 
-        # 영상 화면 띄우기
-        cv2.imshow('RealSense RGB (subscriber)', frame)
-        cv2.waitKey(1)
+            frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            if frame is not None:
+                cv2.imshow('RealSense Compressed RGB (subscriber)', frame)
+                cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f'Could not decode image: {e}')
+      
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = RealSenseRGBSubscriber()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
-    cv2.destroyAllWindows()
-
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+        cv2.destroyAllWindows()
+  
 
 if __name__ == '__main__':
     main()
--- avatar_vision/setup.py
+++ avatar_vision/setup.py
@@ -26,6 +26,7 @@ setup(
         'console_scripts': [
             'rgb_subscriber = avatar_vision.rgb_sub:main',
             'realsense_rgb_publisher = avatar_vision.camera_test:main',
+            'webcam_pub = avatar_vision.webcam_pub:main',
         ],
     },
 )
